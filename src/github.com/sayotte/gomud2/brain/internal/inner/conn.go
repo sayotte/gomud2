77e5ce22a2800929d99c82090fbc666f8bc5c31e
Stephen Ayotte <stephen.ayotte@gmail.com>
2019-02-28 15:43:51 -0500

    package innerbrain -> inner

--- src/github.com/sayotte/gomud2/brain/internal/inner/conn.go
+++ src/github.com/sayotte/gomud2/brain/internal/inner/conn.go
@@ -1,4 +1,4 @@
-package innerbrain
+package inner
 
 import (
 	"encoding/json"
--- src/github.com/sayotte/gomud2/brain/internal/inner/memorykeys.go
+++ src/github.com/sayotte/gomud2/brain/internal/inner/memorykeys.go
@@ -1,4 +1,4 @@
-package innerbrain
+package inner
 
 const (
 	memoryMyCurrentLocationID   = "my-current-location-id"
